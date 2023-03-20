5b573359aa334b44227f55497321ba1cfa25da00
BuildTools <unconfigured@null.spigotmc.org>
2023-03-20 10:31:35 +0300

    Расчёт средних просмотров как в тгстат. Добавелны значения для вставки в док с метриками

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import StatesGroup, State
-import os
+import sys
 import keyboard
 import logging
 from telethon.sync import TelegramClient
@@ -19,10 +19,12 @@ import keep_alive
 keep_alive.keep_alive()
 
 
+#Раскомментировать перед отправкой в продкашен
 phone = os.environ['PHONE']
 api_id = int(os.environ['API_ID'])
 api_hash = os.environ['API_HASH']
 botkey = os.environ['BOTKEY']
+
 
 storage = MemoryStorage()  # FOR FSM
 bot = Bot(token=botkey, parse_mode=types.ParseMode.HTML)
@@ -207,10 +209,11 @@ async def get_address(msg: types.Message, state: FSMContext):
   #забираем столбец с отписками
   start = membersStr.find('"y1",') + 5
   end = membersStr.find(']', start)
-  joinedMembersList = [int(i) for i in membersStr[start:end].split(',')]
+  unsubMembersList = [int(i) for i in membersStr[start:end].split(',')]
   weekday = int(datetime.today().isoweekday())
   startIndexOfMemberList = 0
   endIndexOfMemberList = 0
+
   #со среды у нас всё ок со счётом. А вот во вт-пн надо считать прерыдущую неделю
   if weekday >= 3:
     startIndexOfMemberList = 0 - (weekday - 2)
@@ -218,13 +221,9 @@ async def get_address(msg: types.Message, state: FSMContext):
   else:
     startIndexOfMemberList = 0 - (weekday + 5)
     endIndexOfMemberList = startIndexOfMemberList - 7
-  sumJoinedMembers = list(
-    map(int, joinedMembersList[endIndexOfMemberList:startIndexOfMemberList]))
-  sumJoinedMembers = sum(sumJoinedMembers)
 
-  msgJoined = "<b>Отписки: </b>" + str(
-    joinedMembersList[endIndexOfMemberList:startIndexOfMemberList]
-  ) + ". \n<b>Всего: </b>" + str(sumJoinedMembers)
+  sumUnsubMembers = sum(list(
+    map(int, unsubMembersList[endIndexOfMemberList:startIndexOfMemberList])))
 
   #забираем столбец с подписками
   start = membersStr.find('"y0",') + 5
@@ -244,10 +243,6 @@ async def get_address(msg: types.Message, state: FSMContext):
     map(int, joinedMembersList[endIndexOfMemberList:startIndexOfMemberList]))
   sumJoinedMembers = sum(sumJoinedMembers)
 
-  msgJoined += "\n\n<b>Подписки: </b>" + str(
-    joinedMembersList[endIndexOfMemberList:startIndexOfMemberList]
-  ) + ". \n<b>Всего: </b>" + str(sumJoinedMembers)
-
   #забираем столбец с общим числом подписчиков
   start = followerStr.find('"y0",') + 5
   end = followerStr.find(']', start)
@@ -264,10 +259,9 @@ async def get_address(msg: types.Message, state: FSMContext):
     endIndexOfMemberList = startIndexOfMemberList - 7
   sumAllMembers = list(
     map(int, allMembersList[endIndexOfMemberList:startIndexOfMemberList]))
-  print("Всего подписчиков: " + str(sumAllMembers))
-  msgJoined += "\n\n<b>Количество подписчиков: </b>" + str(sumAllMembers[-1])
-  print("Начало индекса: " + str(startIndexOfMemberList))
-  print("Конец индекса: " + str(endIndexOfMemberList))
+  sumAllMembers = sumAllMembers[-1]
+  
+  #считаем реакции
   try:
     async for message in client.iter_messages(chat,
                                               reverse=True,
@@ -331,10 +325,8 @@ async def get_address(msg: types.Message, state: FSMContext):
         countReactOnP = list(map(int, countReactOnP))
         countReactOnP = sum(countReactOnP)
 
-        print("\n\nПросмотров у поста?: " + str(message.views))
+        print("\n\nПросмотров у поста: " + str(message.views))
         countViews.append(int(message.views))
-        print("Все просмотры, которые есть?: ")
-        print(*countViews)
 
         #Пробуем зайти в комменты
         try:
@@ -381,14 +373,23 @@ async def get_address(msg: types.Message, state: FSMContext):
         allReactions.append(int(countAllReactMP))
 
     try:
-      startDate = datetime.now() - timedelta(days=30)
-      endDate = datetime.now()
-      #Присваиваем таймзону, чтобы не было проблем при сравнении с message.date
+      #Присваиваем даты для подсчёта постов за месяц
+      weekday = int(datetime.today().isoweekday())
+      if weekday >= 3:
+        startDate = datetime.now() - timedelta(days=datetime.today().weekday() +
+                                              30)
+        endDate = datetime.now() - timedelta(days=datetime.today().weekday())
+        print("Сейчас среда, четверг, пятница, суббота или воскресенье. Считаем даты от " + str(startDate) + " и до " + str(endDate))
+      else:
+        startDate = datetime.now() - timedelta(days=datetime.today().weekday() +
+                                              34)
+        endDate = datetime.now() - timedelta(days=datetime.today().weekday() + 4)
+        print("Сейчас понедельник или вторник. Считаем даты от " + str(startDate) + " и до " + str(endDate))
 
+      #Присваиваем таймзону, чтобы не было проблем при сравнении с message.date
       startDate = startDate.replace(hour=0, minute=0, second=0, microsecond=0)
       endDate = endDate.replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=timezone.utc)
       prvGroupID = 0
-      countViews = []
 
       try:
         async for message in client.iter_messages(chat, reverse=True,offset_date=startDate):
@@ -421,28 +422,48 @@ async def get_address(msg: types.Message, state: FSMContext):
                     #если айди разные, значит это первый пост из группы, запоминаем айди
                 else:
                     prvGroupID = message.grouped_id
-
-              print("\n\nПросмотров у поста: " + str(message.views))
               countViews.append(int(message.views))
-              print("Все просмотры, которые есть?: ")
-              print(*countViews)
       
-          countPostsForSrViews = len(countViews)
-          print("\n\nКоличетство контентных постов: " + str(countPostsForSrViews))
-          countViewsSr = list(map(int, countViews))
-          countViewsSr = sum(countViewsSr)
-          print("Всего просмотров постов: " + str(countViewsSr))
-          viewsSr = countViewsSr // countPostsForSrViews
-          print("Среднее количество просмотров на пост: " + str(viewsSr))
+        countPostsForSrViews = len(countViews)
+        print("\n\nКоличество контентных постов: " + str(countPostsForSrViews))
+        countViewsSr = list(map(int, countViews))
+        countViewsSr = sum(countViewsSr)
+        print("Всего просмотров постов: " + str(countViewsSr))
+        viewsSr = countViewsSr // countPostsForSrViews
+        print("Среднее количество просмотров на пост: " + str(viewsSr))
 
       except Exception as e:
-          errorMsg = "Почему-то не получилось собрать посты. Ошибка: " + str(e)
+          errorMsg = "Почему-то не получилось собрать посты. Ошибка: " + str(e) + "\n\nError on line {}".format(sys.exc_info()[-1].tb_lineno)
           await bot.send_message(msg.from_user.id, errorMsg)
           await state.finish()
+      
       print("Формирую финальное сообщение...")
-      statPostMed = "<b>Итоговая статистика</b>\n\n" + msgJoined + "\n\n<b>(Beta) Средний охват поста: </b>" + str(
-        viewsSr) + "\n\n<b>Медиана реакций: </b>" + str(
+      #считаем процент отписок и подписок и добавляем в сообщение
+      msgJoined = "<b>Отписки: </b>" + str(sumUnsubMembers)
+      msgJoined += "\n<b>Подписки: </b>" + str(sumJoinedMembers)
+      procUnsubDay = round((sumUnsubMembers/sumAllMembers/7)*100, 2)
+      msgJoined += "\n<b>Отписок в день: </b>" + str(procUnsubDay) + "%"
+      procJoinedDay = round((sumJoinedMembers/sumAllMembers/7)*100, 2)
+      msgJoined += "\n<b>Подписок в день: </b>" + str(procJoinedDay) + "%"
+
+      #считаем долю активной аудитории
+      msgJoined += "\n<b>Количество подписчиков: </b>" + str(sumAllMembers)
+      msgJoined += "\n<b>Средний охват поста: </b>" + str(
+        viewsSr)
+      activeAudit = round((viewsSr/sumAllMembers)*100, 2)
+      msgJoined += "\n<b>Доля активной аудитории: </b>" + str(
+        activeAudit) + "%"
+      
+      #считаем долю вовлечённой аудитории
+      msgJoined += "\n<b>Медиана реакций: </b>" + str(
           statistics.median(allReactions))
+      engagedAudit = round((statistics.median(allReactions)/viewsSr)*100, 2)
+      msgJoined += "\n<b>Доля вовлечённой аудтиории: </b>" + str(engagedAudit) + "%"
+      
+      statToPaste = "\n\n<b>Значения для вставки:</b>\n<code>" + str(sumUnsubMembers) + "\n" + str(sumJoinedMembers) + "\n" + str(procUnsubDay) + "%" + "\n" + str(procJoinedDay) + "%" + "\n" + str(sumAllMembers) + "\n" + str(viewsSr) + "\n" + str(activeAudit) + "%" + "\n" + str(
+          statistics.median(allReactions)) + "\n" + str(engagedAudit) + "%</code>"
+
+      statPostMed = "<b>Итоговая статистика</b>\n\n" + msgJoined + statToPaste
       
       print("Обрезаю сообщения, чтобы уместились в 4096 символов...")
       # режем сообщения больше 4096 символов, телеграм больше не примет
@@ -461,7 +482,7 @@ async def get_address(msg: types.Message, state: FSMContext):
 
     except Exception as e:
       statPostMed = "Почему-то не получилось собрать медиану\nОшибка: " + str(
-        e)
+        e) + "\n\nError on line {}".format(sys.exc_info()[-1].tb_lineno)
       if len(statPost) > 4096:
         for x in range(0, len(statPost), 4096):
           await bot.send_message(msg.from_user.id,
